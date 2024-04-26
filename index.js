e4804f916c820c13c14368028ce36c5f9f763b55
Naveed GAIE <160273630+NaveedGenAIE@users.noreply.github.com>
2024-04-26 23:43:15 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+let currency = {
+    USD: 1,
+    EUR: 0.97,
+    CHF: 0.93,
+    GBP: 0.79,
+    CAD: 1.28,
+};
+let userAns = await inquirer.prompt([
+    {
+        name: "from",
+        type: "list",
+        message: "Enter your from currency",
+        choices: ["USD", "EUR", "CHF", "GBP", "CAD"],
+    },
+    {
+        name: "to",
+        type: "list",
+        message: "Enter your to currency",
+        choices: ["USD", "EUR", "CHF", "GBP", "CAD"],
+    },
+    {
+        name: "Amount",
+        type: "number",
+        message: "Enter your Amount",
+    },
+]);
+let fromAmount = currency[userAns.from];
+let toAmount = currency[userAns.to];
+let amount = userAns.Amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
